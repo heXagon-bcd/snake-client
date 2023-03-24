0df07d0b9e832dfb906205a45f23ee1fa4f33873
Shawn He <shawnhe1991@gmail.com>
2023-03-24 18:12:27 -0400

    process.exit implement

--- play.js
+++ play.js
@@ -2,3 +2,21 @@
   const connect = require("./client");
 
   connect();
+
+  const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    stdin.on("data", handleUserInput);
+    const handleUserInput = function () {
+    stdin.on("data", (key) => {
+      if (key === '\u0003') {
+        process.exit();
+      }
+    })
+    };
+
+    return stdin;
+  };
+
