4712edaee06208274019c3df792940c1c71169a9
Shawn He <shawnhe1991@gmail.com>
2023-03-24 10:52:03 -0400

    complet module

--- play.js
+++ play.js
@@ -1,27 +1,4 @@
-const net = require("net");
+  const net = require("net");
+  const connect = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",// IP address here,
-    port: 50541,// PORT number here,
-
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log("You are connected!");
-  });
-
-  conn.on('data', () => {
-    console.log('you died because you are idle')});
-
-  return conn;
-};
-
-console.log("Connecting ...");
-
-connect();
-
+  connect();
