43cda74316f3b8f887b9278d7a1bf2bbf93b98bd
Shawn He <shawnhe1991@gmail.com>
2023-03-30 00:24:34 -0400

    lint client

--- client.js
+++ client.js
@@ -1,12 +1,13 @@
+//This module handles the connection with the server
+
 const net = require("net");
 const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: IP,// IP address here,
-    port: PORT,// PORT number here,
-
+    port: PORT,// PORT number here,s
   });
 
   // interpret incoming data as text
@@ -15,18 +16,11 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("You are connected!");
     conn.write("Name: heX");
-  //   setTimeout(() => {conn.write("Move: up")}, 1000);
-  //   setTimeout(() => {conn.write("Move: up")}, 2000);
-  //   setTimeout(() => {conn.write("Move: up")}, 3000);
-  //   const hello = setInterval(() => {
-  //     conn.write("Move: up")
-  //   }, 500);
-  //  setTimeout(() => {clearInterval(hello)},5000);
   });
 
   conn.on('data', () => {
-    console.log('you died because you are idle')});
-
+    console.log('you died because you are idle');
+  });
   return conn;
 };
 
